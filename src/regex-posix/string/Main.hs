502e41fc996f32770585edacfe8d8bca7035bb49
Juliana Restrepo Tobar <60147041+jrestrepot@users.noreply.github.com>
2021-05-30 19:24:13 -0500

    Add files via upload

---
+++ src/regex-posix/string/Main.hs
@@ -0,0 +1,23 @@
+-- Regex-Posix String
+module Main where
+
+import System.Environment
+import Data.List
+import Data.Time
+import Text.Regex.Posix
+
+
+-- First, Main reads two arguments through the command line, the file and the regular expression.
+-- Then, Main prints whether the file matches the regex and how much time did it spend excecuting.
+main :: IO ()
+main = do
+    file <- head <$> getArgs
+    regex <- head . tail <$> getArgs
+    content <- readFile file
+    start <- getCurrentTime
+    let result = content =~ regex :: Bool
+    now <- getCurrentTime
+    let exec_time = diffUTCTime now start
+    print(result)
+    print(exec_time)
+    
