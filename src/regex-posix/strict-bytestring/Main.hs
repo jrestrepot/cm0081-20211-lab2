88658f714b7a62774337e7c3a0e75e9a04da23ed
Juliana Restrepo Tobar <60147041+jrestrepot@users.noreply.github.com>
2021-05-30 19:24:45 -0500

    Add files via upload

---
+++ src/regex-posix/strict-bytestring/Main.hs
@@ -0,0 +1,21 @@
+-- Regex-Posix Bytestring Strict
+module Main where
+
+import System.Environment
+import Data.List
+import Data.Time
+import Text.Regex.Posix.ByteString
+import Text.Regex.Posix
+import qualified Data.ByteString as BS
+
+main :: IO ()
+main = do
+    file <- head <$> getArgs
+    regex <- head . tail <$> getArgs
+    content <- BS.readFile file
+    start <- getCurrentTime
+    let result = content =~ regex :: Bool
+    now <- getCurrentTime
+    let exec_time = diffUTCTime now start
+    print(result)
+    print(exec_time)
