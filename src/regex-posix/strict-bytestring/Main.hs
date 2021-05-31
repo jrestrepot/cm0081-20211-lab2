f671489f7c966011002900bd79361b6d55da0bc4
Juliana Restrepo Tobar <60147041+jrestrepot@users.noreply.github.com>
2021-05-31 12:31:30 -0500

    Update Main.hs

--- src/regex-posix/strict-bytestring/Main.hs
+++ src/regex-posix/strict-bytestring/Main.hs
@@ -8,12 +8,12 @@ import qualified Data.ByteString as BS
 
 main :: IO ()
 main = do
+    start <- getCurrentTime
     file <- head <$> getArgs
     regex <- head . tail <$> getArgs
     content <- BS.readFile file
-    start <- getCurrentTime
     let result = content =~ regex :: Bool
     now <- getCurrentTime
     let exec_time = diffUTCTime now start
-    print(result)
-    print(exec_time)
+    putStrLn("Result: " ++ (show result))
+    putStrLn("Time: " ++ (show exec_time))
