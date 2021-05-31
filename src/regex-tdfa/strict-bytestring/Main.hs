7b9ab88e94382b4a7f01c3cabb449dbb6e40c823
Juliana Restrepo Tobar <60147041+jrestrepot@users.noreply.github.com>
2021-05-30 19:19:02 -0500

    Add files via upload

---
+++ src/regex-tdfa/strict-bytestring/Main.hs
@@ -0,0 +1,20 @@
+-- Regex-Tdfa ByteString
+module Main where
+import System.Environment
+import Text.Regex.TDFA.ByteString
+import Text.Regex.TDFA
+import Data.List
+import Data.Time
+import qualified Data.ByteString as BS
+
+main :: IO()
+main = do
+    file <- head <$> getArgs
+    regex <- head . tail <$> getArgs
+    content <- BS.readFile file
+    start <- getCurrentTime
+    let result = content =~ regex :: Bool
+    now <- getCurrentTime
+    let exec_time = diffUTCTime now start
+    print(result)
+    print(exec_time)
