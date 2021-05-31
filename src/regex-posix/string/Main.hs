135f62263e1c07f5a778737f97218631974f1db8
Juliana Restrepo Tobar <60147041+jrestrepot@users.noreply.github.com>
2021-05-31 12:30:15 -0500

    Update Main.hs

--- src/regex-posix/string/Main.hs
+++ src/regex-posix/string/Main.hs
@@ -5,18 +5,16 @@ import System.Environment
 import Data.Time
 import Text.Regex.Posix
 
-
 -- First, Main reads two arguments through the command line, the file and the regular expression.
 -- Then, Main prints whether the file matches the regex and how much time did it spend excecuting.
 main :: IO ()
 main = do
+    start <- getCurrentTime
     file <- head <$> getArgs
     regex <- head . tail <$> getArgs
     content <- readFile file
-    start <- getCurrentTime
     let result = content =~ regex :: Bool
     now <- getCurrentTime
     let exec_time = diffUTCTime now start
-    print(result)
-    print(exec_time)
-    
+    putStrLn("Result: " ++ (show result))
+    putStrLn("Time: " ++ (show exec_time))
